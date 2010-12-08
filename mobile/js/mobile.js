fb149eb0e137de8149429a573df98dfec8fe1f4f
zomie <zomie1@gmail.com>
2010-12-08 02:41:17 -0800

    added state change for buttons and feed update

--- mobile/js/mobile.js
+++ mobile/js/mobile.js
@@ -1,22 +1,35 @@
 YUI().use("yql", "node", function(Y) {
  
     var res = Y.one('#main'),
-    sources = Y.all('button'),
-    srcStr = 'http://pipes.yahoo.com/zomie/dealwad?_render=json';
-    for(var x = 0;x<=(sources._nodes.length - 1);x++) {
-    		srcStr += '&url' + x + '=' + sources._nodes[x].value;
-    }
-    console.log(sources);
-    console.log(srcStr);
-Y.YQL('select * from json where url="'+srcStr+'"', function(r) {
-       var mixedResults = r.query.results.json.value.items;
-       var html = '';
-       console.log(mixedResults);
-       for (var y = 0;y<=(mixedResults.length - 1);y++) {
+    sources = Y.all('button');
+    var handleClick = function (e) {
+    	
+    	Y.all('button').setAttribute('class','off');
+    	e.target.setAttribute('class','on');
+    	loadFeed();
+    	
+    };
+    Y.on("click", handleClick, ["header button"]);
+
+    var loadFeed = function () {
+        var srcStr = 'http://pipes.yahoo.com/zomie/dealwad?_render=json';
+        for(var x = 0;x<=(sources._nodes.length - 1);x++) {
+            if (sources._nodes[x].getAttribute('class')!='off') {
+    	       srcStr += '&url' + x + '=' + sources._nodes[x].value;
+    	    }
+    	}
+    	console.log(srcStr);
+    	Y.YQL('select * from json where url="'+srcStr+'"', function(r) {
+      	    var mixedResults = r.query.results.json.value.items;
+      	    var html = '';
+            for (var y = 0;y<=(mixedResults.length - 1);y++) {
        		var title = mixedResults[y].title;
        		html += "<div><h4>" + title + "</h4><p>" + mixedResults[y].pubDate + "</p></div>";
-       }
-       res.set('innerHTML', html);
-    });
+            }
+         res.set('innerHTML', html);
+        });
+    };
+    
+    loadFeed();
  
 });
